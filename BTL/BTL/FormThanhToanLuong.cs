39829b4378cd31eb80f7ce012b4220ac81efe6ea
nam <bethanhnam12002@gmail.com>
2022-10-12 17:54:02 +0700

    formthanhtoanluong

---
+++ BTL/BTL/FormThanhToanLuong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL
+{
+    public partial class FormThanhToanLuong : Form
+    {
+        public FormThanhToanLuong()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ThuongThem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
