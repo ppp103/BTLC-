d9470e6c9ae41a97d685e208ba084a06ee9187d0
nam <bethanhnam12002@gmail.com>
2022-10-13 22:20:07 +0700

    13/10 sửa listview (nam)

--- BTL/BTL/FormThanhToanLuong.cs
+++ BTL/BTL/FormThanhToanLuong.cs
@@ -36,5 +36,42 @@ namespace BTL
         {
 
         }
+
+        private void FormThanhToanLuong_Load(object sender, EventArgs e)
+        {
+            DanhSachLuong.View = View.Details;
+            DanhSachLuong.GridLines = true;
+            DanhSachLuong.FullRowSelect = true;
+
+            //Thêm tiêu đề cho cột
+
+            DanhSachLuong.Columns.Add("StaffCode", 100);
+            DanhSachLuong.Columns.Add("StaffName", 100);
+            DanhSachLuong.Columns.Add("HoursWork", 100);
+            DanhSachLuong.Columns.Add("Bonus", 100);
+            DanhSachLuong.Columns.Add("TotalSalary", 100);
+
+            //Thêm Item vào listview
+            string[] arr = new string[5];
+            ListViewItem itm;
+
+            //Thêm Item đầu tiên
+            arr[0] = "001";
+            arr[1] = "nam";
+            arr[2] = "1000";
+            arr[3] = "900 VND";
+            arr[4] = "1.900.000 VND";
+            itm = new ListViewItem(arr);
+            DanhSachLuong.Items.Add(itm);
+
+            //Thêm Item thứ 2
+            arr[0] = "002";
+            arr[1] = "hen";
+            arr[2] = "2000";
+            arr[3] = "700 VND";
+            arr[4] = "1.700.000 VND";
+            itm = new ListViewItem(arr);
+            DanhSachLuong.Items.Add(itm);
+        }
     }
 }
