e88054e48ddfc45ce306d9f02982a773a41eb6f2
AnAn <AnAn@LAPTOP-AUDFFK8M>
2022-10-19 09:42:13 +0700

    an

--- BTL/BTL/Form1.Designer.cs
+++ BTL/BTL/Form1.Designer.cs
@@ -36,25 +36,26 @@ namespace BTL
             this.btnThongKeHangHoa = new System.Windows.Forms.Button();
             this.btnThanhToanLuong = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel7 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
-            this.panel9 = new System.Windows.Forms.Panel();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.panel10 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel9.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -64,7 +65,7 @@ namespace BTL
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(-1, 93);
+            this.button1.Location = new System.Drawing.Point(-1, 127);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(221, 40);
             this.button1.TabIndex = 4;
@@ -129,14 +130,48 @@ namespace BTL
             this.panel1.Controls.Add(this.panel9);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(224, 93);
+            this.panel1.Location = new System.Drawing.Point(218, 93);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(998, 615);
+            this.panel1.Size = new System.Drawing.Size(1004, 615);
             this.panel1.TabIndex = 5;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.LightCoral;
+            this.panel9.Controls.Add(this.button2);
+            this.panel9.Controls.Add(this.label7);
+            this.panel9.Location = new System.Drawing.Point(0, 564);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(630, 51);
+            this.panel9.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(17, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 51);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.LightCoral;
+            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(70, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(555, 26);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "TƯ VẤN MIỄN PHÍ 24/7 - HOTLINE :0364709903 0395452604";
             // 
             // panel3
             // 
             this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel3.Location = new System.Drawing.Point(1, 42);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(630, 524);
@@ -161,6 +196,75 @@ namespace BTL
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(366, 615);
             this.panel2.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(118, 578);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Thanh Toán Online";
+            // 
+            // panel8
+            // 
+            this.panel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel8.BackgroundImage")));
+            this.panel8.Location = new System.Drawing.Point(90, 451);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(174, 115);
+            this.panel8.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(178, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(182, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Thanh Toán Khi Nhận Hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 412);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Giao Hàng Toàn Quốc";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(203, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cam Kết Chất Lượng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hàng Chính Hãng 100%";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(65, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UY TÍN - CHẤT LƯỢNG";
             // 
             // panel7
             // 
@@ -195,107 +299,13 @@ namespace BTL
             this.panel4.Size = new System.Drawing.Size(174, 115);
             this.panel4.TabIndex = 0;
             // 
-            // label1
+            // panel10
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(65, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(236, 26);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "UY TÍN - CHẤT LƯỢNG";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 152);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 18);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Hàng Chính Hãng 100%";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(203, 152);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 18);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Cam Kết Chất Lượng";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 412);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 18);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Giao Hàng Toàn Quốc";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(178, 412);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(182, 18);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Thanh Toán Khi Nhận Hàng";
-            // 
-            // panel8
-            // 
-            this.panel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel8.BackgroundImage")));
-            this.panel8.Location = new System.Drawing.Point(90, 451);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(174, 115);
-            this.panel8.TabIndex = 6;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(118, 578);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(128, 18);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "Thanh Toán Online";
-            // 
-            // panel9
-            // 
-            this.panel9.BackColor = System.Drawing.Color.LightCoral;
-            this.panel9.Controls.Add(this.button2);
-            this.panel9.Controls.Add(this.label7);
-            this.panel9.Location = new System.Drawing.Point(0, 564);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(630, 51);
-            this.panel9.TabIndex = 2;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.LightCoral;
-            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(70, 14);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(555, 26);
-            this.label7.TabIndex = 1;
-            this.label7.Text = "TƯ VẤN MIỄN PHÍ 24/7 - HOTLINE :0364709903 0395452604";
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.LightCoral;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(17, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(60, 51);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = false;
+            this.panel10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel10.BackgroundImage")));
+            this.panel10.Location = new System.Drawing.Point(-1, 0);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(219, 93);
+            this.panel10.TabIndex = 6;
             // 
             // Form1
             // 
@@ -303,6 +313,7 @@ namespace BTL
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1214, 708);
+            this.Controls.Add(this.panel10);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnThanhToanLuong);
@@ -315,10 +326,10 @@ namespace BTL
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản Lí Bán Nước Hoa";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel9.ResumeLayout(false);
             this.panel9.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -346,6 +357,7 @@ namespace BTL
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel10;
     }
 }
 
