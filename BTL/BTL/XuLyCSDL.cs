34e8e5a7a2f1d1581a9c2b255043e72796725a23
NTAnhTuyet268 <92579241+NTAnhTuyet268@users.noreply.github.com>
2022-11-13 14:43:13 +0700

    â

--- BTL/BTL/XuLyCSDL.cs
+++ BTL/BTL/XuLyCSDL.cs
@@ -10,7 +10,7 @@ namespace BTL
 {
     internal class XuLyCSDL
     {
-        string strConnect = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\anhtu\OneDrive\Documents\GitHub\BTLC-\BTL\BTL\DataBase\DuLieu.mdf;Integrated Security=True" + System.IO.Directory.GetCurrentDirectory().ToString() + "\\DataBase\\" + "DuLieu.mdf;Integrated Security=True";
+        string strConnect = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename= Data Source=.\\SQLEXPRESS;AttachDbFilename=" + System.IO.Directory.GetCurrentDirectory().ToString() + "\\DataBase\\" + "DuLieu.mdf;Integrated Security=True";
         SqlConnection sqlConnect = null;
 
         //Hàm mở kết nối CSDL
