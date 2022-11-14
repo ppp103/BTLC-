f071af856b2d1121b03436de0b0383ac18072289
Phuc <trandinhphuc1410@gmail.com.vn>
2022-11-14 16:14:43 +0700

    commit

--- BTL/BTL/Program.cs
+++ BTL/BTL/Program.cs
@@ -16,8 +16,8 @@ namespace BTL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new DangNhapDangKi.DangNhap());
-            Application.Run(new HomePage.TrangChinh());
+            Application.Run(new DangNhapDangKi.DangNhap());
+            //Application.Run(new HomePage.TrangChinh());
             //Application.Run(new Forms.BaoCao());
         }
     }
