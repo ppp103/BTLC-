2c3f0d7ccd36b9e4bedf035369046784dd3ea00f
Phuc <trandinhphuc1410@gmail.com.vn>
2022-11-15 15:43:57 +0700

    commit

--- BTL/BTL/Forms/BaoCao.cs
+++ BTL/BTL/Forms/BaoCao.cs
@@ -13,31 +13,85 @@ namespace BTL.Forms
     public partial class BaoCao : Form
     {
         XuLyCSDL xuLy = new XuLyCSDL();
+        int index;
         public BaoCao()
         {
             InitializeComponent();
         }
 
+        private bool check()
+        {
+            if(txtInput.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầu vào");
+                return false;
+            }
+            return true;
+        }
+
         private void BaoCao_Load(object sender, EventArgs e)
         {
-            //guna2DataGridView1.Columns[0].
+            index = 1;
+            dateTimePicker.CustomFormat = "MMMM yyyy";
+            lbTgian.Visible = false;
+            dateTimePicker.Visible = false;
         }
 
         private void sảnPhẩmBánChạyNhấtTừNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            index = 1;
+            dateTimePicker.Visible = false;
+            lbTgian.Visible = false;
+            lbInput.Text = "Mã NV";
         }
 
-        private void guna2Button1_Click(object sender, EventArgs e)
+        private void danhSáchHĐToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(txtMaNV.Text == "")
+            index = 2;
+            lbInput.Text = "Mã NCC";
+            lbTgian.Visible = true;
+            dateTimePicker.Visible = true;
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            if(index == 1 && check())
             {
-                MessageBox.Show("Vui lòng nhập mã nhân viên");
-            }
-            else
-            {
-                guna2DataGridView1.DataSource = xuLy.DocBang($"Select * From BaoCao1('{txtMaNV.Text}')");
+                DataTable table = xuLy.DocBang($"Select * From BaoCao1('{txtInput.Text}')");
+                
+                if(table.Rows.Count > 0)
+                {
+                    MessageBox.Show("Tìm thấy dữ liệu");
+                    dgvKetQua.DataSource = table;
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy dữ liệu");
+                }
             }
 
+            if(index == 2 && check())
+            {
+                int thang = dateTimePicker.Value.Month;
+                int nam = dateTimePicker.Value.Year;
+                DataTable table = xuLy.DocBang($"Select * From BaoCao2('{txtInput.Text}', {thang}, {nam})");
+
+                if (table.Rows.Count > 0)
+                {
+                    MessageBox.Show("Tìm thấy dữ liệu");
+                    dgvKetQua.DataSource = table;
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy dữ liệu");
+                }
+            }
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            dateTimePicker.Value = DateTime.Today;
         }
     }
 }
