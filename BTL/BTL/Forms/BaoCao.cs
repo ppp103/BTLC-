494750bf9ea41ab738a4f33c07a00524d0b2e5c9
Phuc <trandinhphuc1410@gmail.com.vn>
2022-11-16 16:46:00 +0700

    commit

--- BTL/BTL/Forms/BaoCao.cs
+++ BTL/BTL/Forms/BaoCao.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace BTL.Forms
 {
@@ -21,9 +22,18 @@ namespace BTL.Forms
 
         private bool check()
         {
-            if(txtInput.Text == "")
+            if(index == 1 || index == 2)
             {
-                MessageBox.Show("Vui lòng nhập đầu vào");
+                if(txtInput.Text == "")
+                {
+                    MessageBox.Show("Vui lòng nhập đầu vào");
+                    return false;
+                }
+            }
+
+            if(index == 3 && cboQuy.SelectedIndex < 0)
+            {
+                MessageBox.Show("Vui lòng chọn quý");
                 return false;
             }
             return true;
@@ -33,54 +43,82 @@ namespace BTL.Forms
         {
             index = 1;
             btnLamMoi_Click(this, e);
-            dateTimePicker.CustomFormat = "MMMM yyyy";
-            lbTgian.Visible = false;
-            lbInput2.Visible = false;
-            cboQuy.Visible = false;
-            dateTimePicker.Visible = false;
+            MenuItem1_Click(this, e);
         }
 
-        private void sảnPhẩmBánChạyNhấtTừNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            dateTimePicker.Value = DateTime.Today;
+            dgvKetQua.DataSource = null;
+            lbOutPut.Text = "Tổng Tiền Nhập Hàng";
+        }
+
+        private void ActivateMenu1(bool state)
+        {
+            lbInput.Visible = state;
+            txtInput.Visible = state;
+        }
+
+        private void ActivateMenu2(bool state)
+        {
+            dateTimePicker.Visible = state;
+            lbTgian.Visible = state;
+            dateTimePicker.CustomFormat = "MMMM yyyy";
+            
+            lbOutPut.Visible = state;
+
+            lbInput.Visible = state;
+            txtInput.Visible = state;
+        }
+
+        private void ActivateMenu3(bool state)
+        {
+            dateTimePicker.CustomFormat = "yyyy";
+            dateTimePicker.Visible = state;
+            lbTgian.Visible = state;
+            
+            cboQuy.Visible = state;
+            lbQuy.Visible = state;
+            
+            lbInput.Visible = !state;
+            txtInput.Visible = !state;
+        }
+
+        private void MenuItem1_Click(object sender, EventArgs e)
         {
             index = 1;
-            btnLamMoi_Click(this, e);
-            dateTimePicker.Visible = false;
-            lbTgian.Visible = false;
             lbInput.Text = "Mã NV";
+            btnLamMoi_Click(this, e);
+            ActivateMenu2(false);
+            ActivateMenu3(false);
+            ActivateMenu1(true);
         }
 
-        private void danhSáchHĐToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MenuItem2_Click(object sender, EventArgs e)
         {
             index = 2;
-            btnLamMoi_Click(this, e);
             lbInput.Text = "Mã NCC";
-            lbTgian.Visible = true;
-            dateTimePicker.Visible = true;
+            btnLamMoi_Click(this, e);
+            ActivateMenu3(false);
+            ActivateMenu1(false);
+            ActivateMenu2(true);
+        }
+
+        private void MenuItem3_Click(object sender, EventArgs e)
+        {
+            index = 3;
+            btnLamMoi_Click(this, e);
+            ActivateMenu1(false);
+            ActivateMenu2(false);
+            ActivateMenu3(true);
         }
 
         private void btnThongKe_Click(object sender, EventArgs e)
         {
-            if(index == 1 && check())
+            if (index == 1 && check())
             {
                 DataTable table = xuLy.DocBang($"Select * From BaoCao1('{txtInput.Text}')");
-                
-                if(table.Rows.Count > 0)
-                {
-                    MessageBox.Show("Tìm thấy dữ liệu");
-                    dgvKetQua.DataSource = table;
-                }
-                else
-                {
-                    MessageBox.Show("Không tìm thấy dữ liệu");
-                    dgvKetQua.DataSource = table;
-                }
-            }
-
-            if(index == 2 && check())
-            {
-                int thang = dateTimePicker.Value.Month;
-                int nam = dateTimePicker.Value.Year;
-                DataTable table = xuLy.DocBang($"Select * From BaoCao2('{txtInput.Text}', {thang}, {nam})");
 
                 if (table.Rows.Count > 0)
                 {
@@ -93,13 +131,37 @@ namespace BTL.Forms
                     dgvKetQua.DataSource = table;
                 }
             }
+
+            if (index == 2 && check())
+            {
+                int thang = dateTimePicker.Value.Month;
+                int nam = dateTimePicker.Value.Year;
+                DataTable table = xuLy.DocBang($"Select * From BaoCao2('{txtInput.Text}', {thang}, {nam})");
+
+                if (table.Rows.Count > 0)
+                {
+                    double tien;
+                    MessageBox.Show("Tìm thấy dữ liệu");
+                    dgvKetQua.DataSource = table;
+                    tien = Convert.ToDouble(table.Rows[0][4]);
+                    lbOutPut.Text = $"Tổng tiền nhập hàng từ {txtInput.Text}: {tien}";
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy dữ liệu");
+                    dgvKetQua.DataSource = table;
+                }
+            }
+
+            if (index == 3 && check())
+            {
+                MessageBox.Show("Alo123");
+                if(cboQuy.SelectedIndex == 0)
+                {
+                    //DataTable table = xuLy.DocBang($"Select * From Quy1('{txtInput.Text}', {thang}, {nam})");
+                }
+            }
         }
 
-        private void btnLamMoi_Click(object sender, EventArgs e)
-        {
-            txtInput.Text = "";
-            dateTimePicker.Value = DateTime.Today;
-            dgvKetQua.DataSource = null;
-        }
     }
 }
